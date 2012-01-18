2e9885224cf8f8b6c657b1e3ec085961e2d2c695
Josh Dinges <jdinges@mines.edu>
2012-01-18 14:35:25 -0700

    ex1.rb (road warrior ruby) done

---
+++ project2/ex1.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+
+#puts "Hello World!"
+#puts "Hello world again"
+#puts "I like typing this!"
+#puts 'Yay, printing!'
+#puts "I'd much rather you 'not'"
+#puts 'I "said" do not touch this'
+puts "Another line"
