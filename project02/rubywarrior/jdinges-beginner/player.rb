17bb2fbd6765215712d0f9f949886fe6c8ce26d6
Josh Dinges <jdinges@mines.edu>
2012-01-22 23:51:26 -0700

    Level six done. Took a while to figure out the logic and some string compares.

--- project02/rubywarrior/jdinges-beginner/player.rb
+++ project02/rubywarrior/jdinges-beginner/player.rb
@@ -1,20 +1,43 @@
 class Player
   @health
+  @hitWall
   def play_turn(warrior)
-    if warrior.feel.empty?
-      puts"health = #{@health}\nwHealth = #{warrior.health}\n"
+    #puts"health = #{@health}\nwHealth = #{warrior.health}\n"
+    #puts"backward = #{warrior.feel(:backward)}\n"
+    #puts"forward = #{warrior.feel}\n"
+    if warrior.feel(:backward).empty? && warrior.feel.empty?
+      
       if taking_damage?(warrior)
-        warrior.walk!
-      elsif @health < 12
+        
+        if warrior.health < 9
+          warrior.walk!(:backward)
+        else
+          warrior.walk!
+        end
+        
+      elsif @health < 20
         warrior.rest!
-      else
+      elsif @hitWall
         warrior.walk!
+      else
+        warrior.walk!(:backward)
       end
-    elsif warrior.feel.captive?
+      
+    elsif "#{warrior.feel(:backward)}".eql? "wall"
+      puts"HIT A MOTHEFUCKING WALL!"
+      @hitWall = true
+      warrior.walk!
+      
+    elsif warrior.feel.captive? 
       warrior.rescue!
+      
+    elsif warrior.feel(:backward).captive?
+      warrior.rescue!(:backward)
+      
     else
       warrior.attack! 
     end
+    
     @health = warrior.health
   end
   
