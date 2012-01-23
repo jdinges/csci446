4cef61ff3fd8e6406f0df44679078b5eb150b236
Josh Dinges <jdinges@mines.edu>
2012-01-22 22:04:57 -0700

    deleted leftovers

--- project1/confirm.js
+++
@@ -1,8 +0,0 @@
-// Simply checks to see if the user inputed anything in
-// text field
-function checkText() {
-	var theText = document.forms["theForm"]["status"].value;
-	if(theText == null || theText == ""){
-		alert("Surely you feel something!");
-	}
-}
