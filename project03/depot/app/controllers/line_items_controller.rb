1dbdd5be2ea44ffa3ffaa0d9693bd216e1a6f40b
Josh Dinges <jdinges@mines.edu>
2012-02-07 21:19:24 -0700

    Added column to db table line item that keeps track of the quantity of a given item in cart. Then updated cart.rb model, line_items_controller.rb and carts/show.html.erb to make use of the quantity value. Additionally, updated db so that any duplicate items in the cart will be summed into one. Lastly made the db able to rollback to old way of doing things.

--- project03/depot/app/controllers/line_items_controller.rb
+++ project03/depot/app/controllers/line_items_controller.rb
@@ -44,7 +44,7 @@ class LineItemsController < ApplicationController
 
     @cart = current_cart
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product.id)
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
