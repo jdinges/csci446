c412f485123b5925a3c27c6fa698de77f61960a6
Josh Dinges <jdinges@mines.edu>
2012-02-08 00:13:04 -0700

    Administrative users now must log into view admin content. Accomplished by using some nifty skip_before_filter magic

--- project03/depot/app/controllers/application_controller.rb
+++ project03/depot/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
+  before_filter :authorize
   private
   
     def current_cart
@@ -9,5 +9,13 @@ class ApplicationController < ActionController::Base
       cart = Cart.create
       session[:cart_id] = cart.id
       cart
+    end
+    
+  protected
+  
+    def authorize
+      unless User.find_by_id(session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+      end
     end
 end
--- project03/depot/config/routes.rb
+++ project03/depot/config/routes.rb
@@ -1,19 +1,22 @@
 Depot::Application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   resources :users
-
   resources :orders
-
   resources :line_items
-
   resources :carts
 
   get "store/index"
-
   resources :products do
     get :who_bought, on: :member
   end
-  
-
+  # ...
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -60,14 +63,15 @@ Depot::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
   root to: 'store#index', as: 'store'
+  # ...
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  # match ':controller(/:action(/:id(.:format)))'
 end
