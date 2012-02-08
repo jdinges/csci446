d15c1dadc063e02051c62ea5328b3109cfe72ccc
Josh Dinges <jdinges@mines.edu>
2012-02-07 20:44:03 -0700

    added line_item scaffold to my app. Updated models to ensure that they're all talking to each other

--- project03/depot/app/models/product.rb
+++ project03/depot/app/models/product.rb
@@ -1,9 +1,21 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
   validates :title, :description, :image_url, presence: true
-    validates :price, numericality: {greater_than_or_equal_to: 0.01}
-    validates :title, uniqueness: true
-    validates :image_url, allow_blank: true, format: {
-      with:    %r{\.(gif|jpg|png)$}i,
-      message: 'must be a URL for GIF, JPG or PNG image.'
-    }
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with:    %r{\.(gif|jpg|png)$}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
+  private
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
