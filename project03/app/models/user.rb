a21092d9e8b7f08d49e1cb3b47c96a39dd8c6d00
Josh Dinges <jdinges@mines.edu>
2012-02-15 23:57:33 -0700

    Moved everything up so that project03 dir is my root

---
+++ project03/app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  has_secure_password
+  
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise "Can't delete last user"
+      end
+    end
+end
---
+++ project03/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'd42e0ede45c4236d38a01479ab4e2a0297398856c0be46788ee71b456fcf7c3acc0ef03071fb84000b58053f689b487b70463f4b8315eb8d5eb05254f9ff6a27'
