2dcdc9e4cd6ef1a9c8b4ea85c6f5beef4a151d23
Josh Dinges <jdinges@mines.edu>
2012-02-20 21:17:06 -0700

    Created app, downloaded icons.

---
+++ project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = '24fe57d793437996444f1022e76d82805f2558b20d8353278a8ba2f3b31594b810d7efa294b6be6a465124e6ea8f0fea846910b261e2f6757dcc41532cd56fb4'
