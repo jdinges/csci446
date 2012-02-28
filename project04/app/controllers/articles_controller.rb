7c17bf1907de70268f129a0261d1796cb8676062
Josh Dinges <jdinges@mines.edu>
2012-02-27 22:53:32 -0700

    Styling done for index. Used overflow:auto and a table class to manage it

---
+++ project04/app/controllers/articles_controller.rb
@@ -0,0 +1,86 @@
+class ArticlesController < ApplicationController
+  # GET /articles
+  # GET /articles.json
+  def index
+    @articles = Article.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @articles }
+    end
+  end
+
+  # GET /articles/1
+  # GET /articles/1.json
+  def show
+    @article = Article.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @article }
+    end
+  end
+
+  # GET /articles/new
+  # GET /articles/new.json
+  def new
+    @article = Article.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @article }
+    end
+  end
+
+  # GET /articles/1/edit
+  def edit
+    @article = Article.find(params[:id])
+    session[:return_to] ||= request.env['HTTP_REFERER']
+  end
+
+  # POST /articles
+  # POST /articles.json
+  def create
+    @article = Article.new(params[:article])
+    @article.edit_count = 0
+    @article.date_created = Time.now.in_time_zone('Arizona').to_date
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to @article, notice: 'Article was successfully created.' }
+        format.json { render json: @article, status: :created, location: @article }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /articles/1
+  # PUT /articles/1.json
+  def update
+    @article = Article.find(params[:id])
+    Article.increment_counter(:edit_count, @article.id)
+    
+    respond_to do |format|
+      if @article.update_attributes(params[:article])
+        format.html { redirect_to session[:return_to], notice: 'Article was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /articles/1
+  # DELETE /articles/1.json
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    respond_to do |format|
+      format.html { redirect_to articles_url }
+      format.json { head :no_content }
+    end
+  end
+end
--- project04/config/routes.rb
+++ project04/config/routes.rb
@@ -1,4 +1,7 @@
 Articles::Application.routes.draw do
+  resources :articles
+  
+  #get "articles#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,7 +52,8 @@ Articles::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+  root :to => "articles#index"
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
