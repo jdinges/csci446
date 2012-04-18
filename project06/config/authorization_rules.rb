768df0ff31147bdd133a4f72f6706554978c8dc5
Josh Dinges <jdinges@mines.edu>
2012-04-17 22:22:20 -0600

    Role behavior nearly complete

--- project06/config/authorization_rules.rb
+++ project06/config/authorization_rules.rb
@@ -3,8 +3,14 @@ authorization do
     has_permission_on [:games, :users], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
   end
   
+  role :user do
+    has_permission_on :games, :to => [:index, :show, :new, :create, :edit, :update]
+    has_permission_on :users, :to => [:show, :edit, :update]
+  end
+  
   role :guest do
     has_permission_on :games, :to => [:index, :show]
     has_permission_on :users_sessions, :to => [:index, :show, :new, :create, :edit, :update, :destroy]
+    has_permission_on :users, :to => [:show, :create, :new]
   end
 end
