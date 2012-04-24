e1168435d083b7651342ce9bd7d79e10fab22149
Josh Dinges <jdinges@mines.edu>
2012-04-23 22:08:25 -0600

    Alerts user when there guess is too low, too high, or just right

--- project07/main.js
+++ project07/main.js
@@ -6,47 +6,76 @@ if (typeof sessionStorage.guessesLeft == 'undefined'){
 if (typeof sessionStorage.secretNumber == 'undefined'){
   sessionStorage.secretNumber = Math.floor(Math.random()*100) + 1;
 }
-$(document).ready(function() {
-  $('form').submit(function(e) {
-    e.preventDefault();
-    //alert("Hello world");
-    if($("input:first").val() == sessionStorage.secretNumber.toString()){
-      // call success function
-    } else if($("input:first").val() > sessionStorage.secretNumber.toString()){
-      $("div#infoUpdate").append("Too Damn High!");
-      $("div#infoUpdate").show("slow");
-      return true;
-    } else {
-      $("div#infoUpdate").append("Too Damn Low!");
-      $("div#infoUpdate").show("slow");
-      return true;
-    }
+// $(document).ready(function() {
+//   $('form').submit(function(e) {
+//     e.preventDefault();
+//     //alert("Hello world");
+//     if($("input:first").val() == sessionStorage.secretNumber.toString()){
+//       // call success function
+//     } else if($("input:first").val() > sessionStorage.secretNumber.toString()){
+//       $("div#infoUpdate").append("Too Damn High!");
+//       $("div#infoUpdate").show("slow");
+//       updateRemainingGuesses();
+//       return true;
+//     } else {
+//       $("div#infoUpdate").append("Too Damn Low!");
+//       $("div#infoUpdate").show("slow");
+//       updateRemainingGuesses();
+//       return true;
+//     }
     
-    return false;
-  });
-});
+//     return false;
+//   });
+// });
 
-$('form#guessTheNumber').submit(function(e) {
-  e.preventDefault();
-  // if($("input:first").val() == sessionStorage.secretNumber.toString()){
-  //   $("div#infoUpdate").append("Correct!").show("slow");
-  //   return true;
-  // } else {
-  //   $("div#infoUpdate").append("Incorrect!").show("slow");
-  //   return false;
-  // }
-  alert("testing...");
-  $("div#infoUpdate").append("Incorrect");
-  $("div#infoUpdate").show("slow");
-  return false;
-});
+// $('form#guessTheNumber').submit(function(e) {
+//   e.preventDefault();
+//   // if($("input:first").val() == sessionStorage.secretNumber.toString()){
+//   //   $("div#infoUpdate").append("Correct!").show("slow");
+//   //   return true;
+//   // } else {
+//   //   $("div#infoUpdate").append("Incorrect!").show("slow");
+//   //   return false;
+//   // }
+//   alert("testing...");
+//   $("div#infoUpdate").append("Incorrect");
+//   $("div#infoUpdate").show("slow");
+//   return false;
+// });
 
 var highScores = new Array([9, "HarryJamesPotter"], [3, "ZedCthulhu"], [2, "NearlyDied"]);
 
 $(function() {
+  if(typeof sessionStorage.previousGuess != 'undefined'){
+    // Decrement logic and function
+    sessionStorage.guessesLeft = sessionStorage.guessesLeft - 1;
+    var lastGuess = parseInt(sessionStorage.previousGuess);
+    var correctAnswer = parseInt(sessionStorage.secretNumber);
+    if(lastGuess == correctAnswer){
+      alert("CORRECT!");
+    } else if(lastGuess > correctAnswer){
+      alert("Too Damn High!");
+    } else {
+      alert("Too Damn LOW");
+    }
+  }
   updateScore(sessionStorage.guessesLeft);
   populateHighScores(highScores);
+
 });
+
+$(document).ready(function() {
+  $('form#guessTheNumber').submit(function(e) {
+    var userGuess = parseInt(document.getElementById('guessTheNumber').guess.value);
+    sessionStorage.previousGuess = userGuess;
+    // if(userGuess == sessionStorage.secretNumber.toString()){
+    //   // Call success function
+    // } else {
+      
+    // }
+  });
+});
+
 
 function populateHighScores(scores) {
   for (var i = 0; i < scores.length; ++i) {
@@ -88,14 +117,9 @@ function updateHelp(message){
 }
 
 function updateRemainingGuesses(){
-  var userGuess = parseInt(document.getElementById('guessTheNumber').guess.value);
-  updateHelp("Incorrect!");
+  //var userGuess = parseInt(document.getElementById('guessTheNumber').guess.value);
+
   sessionStorage.guessesLeft = sessionStorage.guessesLeft - 1;
   updateScore(sessionStorage.guessesLeft);
-  if(userGuess == parseInt(sessionStorage.secretNumber)){
-    alert("You guessed correctly!");
-  } else {
-    updateHelp("Incorrect!");
-  }
-  //alert(sessionStorage.guessesLeft);
+
 }
